a1e435646ca7d833e1bbcfe2ed2999606885f0ae
s7manth <syalamarty002@gmail.com>
2023-01-15 06:48:34 +0800

    add footer restart button w/ hardik

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,11 @@
 export default function Footer() {
   return (
     <>
+    <div style={{ color: 'white', marginBottom: '-5px', marginTop: '-10px', cursor: 'pointer' }} onClick={() => location.reload()}>
+        <h2>
+        <i className="fas fa-redo-alt" title="Refresh"></i>
+        </h2>
+    </div>
     <div className={`bottom-area`}>
       <div className="hint">
         <kbd>Enter</kbd> Go to Next Code
