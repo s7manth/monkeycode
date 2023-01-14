6caadddaa4876d2517f3772e6031cb87575933ba
s7manth <syalamarty002@gmail.com>
2023-01-15 04:03:36 +0800

    resolve conflicts

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-
-=======
-// import { useState, useEffect } from 'react'
-
-// export default function Footer() {
-//   const handleRestart = () => {
-//     // Code to handle restarting here
-//   }
-
-// export default function Footer() {
-//   return (
-//     <div className={`bottom-area`}>
-//       <button onClick={handleRestart}>Restart</button>
-//       <span className="hint">
-//         <kbd>Enter</kbd> Go to Next Code
-//       </span>
-//     </div>
-//   )
-// }
-
-import { useState, useEffect } from 'react'
->>>>>>> main
-
 export default function Footer() {
   return (
     <div className={`bottom-area`}>
