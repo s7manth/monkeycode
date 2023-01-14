3d1c63d9fa852a52569b4c48c9f7b1fb3ddb1454
s7manth <syalamarty002@gmail.com>
2023-01-14 19:17:34 +0800

    add metrics

---
+++ components/TypingEditor/metricsHelper.js
@@ -0,0 +1,39 @@
+characters_per_word = {
+    "languages": [
+      {
+        "name": "C++",
+        "avg_characters_per_word": 3.5
+      },
+      {
+        "name": "C#",
+        "avg_characters_per_word": 2.5
+      },
+      {
+        "name": "Java",
+        "avg_characters_per_word": 3
+      },
+      {
+        "name": "Python",
+        "avg_characters_per_word": 1.5
+      },
+      {
+        "name": "JavaScript",
+        "avg_characters_per_word": 2.5
+      },
+      {
+        "name": "Ruby",
+        "avg_characters_per_word": 1.5
+      },
+      {
+        "name": "Go",
+        "avg_characters_per_word": 2
+      },
+      {
+        "name": "Swift",
+        "avg_characters_per_word": 2.5
+      }
+    ]
+  }
+
+export default characters_per_word;
+  
